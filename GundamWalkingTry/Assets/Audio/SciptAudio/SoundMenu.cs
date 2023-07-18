a58256216764fe7a1082b84262af984ba78f78e4
LenionK <karotsalvo@gmail.com>
2023-07-18 10:51:57 +0200

    Audio gui
    fix interpolation

--- GundamWalkingTry/Assets/Audio/SciptAudio/SoundMenu.cs
+++ GundamWalkingTry/Assets/Audio/SciptAudio/SoundMenu.cs
@@ -6,9 +6,20 @@ using UnityEngine.Audio;
 public class SoundMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
-    
+
     public void SetVolume (float volume){
         audioMixer.SetFloat("volume", volume);
     }
-    
+
+    public void SetVolumeEffects(float volume)
+    {
+        audioMixer.SetFloat("Effects", Mathf.Log10(volume)*20);
+    }
+
+    public void SetVolumeMusic(float volume)
+    {
+        audioMixer.SetFloat("Music", Mathf.Log10(volume) * 20);
+    }
+
+
 }
