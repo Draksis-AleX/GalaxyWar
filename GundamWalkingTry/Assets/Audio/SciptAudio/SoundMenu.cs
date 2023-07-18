862c6ba5302a4c1fece7f94e45473cf164facc58
LenionK <karotsalvo@gmail.com>
2023-07-18 11:19:04 +0200

    Audio Menu
    fix

--- GundamWalkingTry/Assets/Audio/SciptAudio/SoundMenu.cs
+++ GundamWalkingTry/Assets/Audio/SciptAudio/SoundMenu.cs
@@ -1,11 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Audio;
 
 public class SoundMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider effectsSlider;
+
+
+    private void Awake()
+    {
+        if (PlayerPrefs.HasKey("Music")) LoadMusic();
+        if (PlayerPrefs.HasKey("Effects")) LoadEffects();
+
+    }
 
     public void SetVolume (float volume){
         audioMixer.SetFloat("volume", volume);
@@ -14,12 +25,20 @@ public class SoundMenu : MonoBehaviour
     public void SetVolumeEffects(float volume)
     {
         audioMixer.SetFloat("Effects", Mathf.Log10(volume)*20);
+        PlayerPrefs.SetFloat("Effects",volume);
     }
 
     public void SetVolumeMusic(float volume)
     {
         audioMixer.SetFloat("Music", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("Music", volume);
     }
 
+    private void LoadMusic() {
+        musicSlider.value = PlayerPrefs.GetFloat("Music");
+    }
+    private void LoadEffects() {
+        effectsSlider.value = PlayerPrefs.GetFloat("Effects");
+    }
 
 }
